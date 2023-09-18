72371a823e1bcaf0928b0b4955e75ce13ef334ba
wande <yewandeadeyemi84@gmail.com>
2023-09-18 23:34:08 +0100

    another commit

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
 
 
-const eye = document.getElementsByClassName("firsteye");
-const sidebar = document.getElementsByClassName("sidenav");
-const isShow = true;
 
-function show() {
-if (isShow){
-   sidebar.style.display = "none";
-   isShow = false;
-}
-}
